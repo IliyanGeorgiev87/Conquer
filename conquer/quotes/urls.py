dd8559b13c763fa919b170f80f7d567e40e14a9d
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-05-06 11:18:26 +0300

    urls, styling, pages

--- conquer/quotes/urls.py
+++ conquer/quotes/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.HomeView, name='home')
+    path('', views.HomeView, name='home'),
+    path('register/', views.RegisterView, name='register'),
+    path('quotes/', views.ListView, name='quotes'),
+    path('login/', views.LoginView,)
 ]
--- conquer/quotes/views.py
+++ conquer/quotes/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def HomeView(request):
     return render(request, "home.html")
+
+def RegisterView(request):
+    return render(request, 'register.html', {})
+
+@login_required
+def ListView(request):
+    return render(request, 'list.html', {})
+
+def LoginView(request):
+    return render(request, 'login.html', {})
